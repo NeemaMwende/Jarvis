f401e8f5036ef6b90289e8e35d20c4ef7412e1ef
Neema Mwende <gracepete12@gmail.com>
2025-11-22 22:33:38 +0300

    system_monitor updates

--- Backend/tools/system_monitor.py
+++ Backend/tools/system_monitor.py
@@ -1,11 +1,14 @@
 import psutil 
 from langchain.tools import tool 
 
-@tool 
+@tool
 def system_status():
     """Returns CPU, RAM and battery status."""
     cpu = psutil.cpu_percent()
     ram = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent 
     
-    return f"CPU: {cpu}%, RAM: {ram}%, Disk: {disk}%"
+    return f"CPU: {cpu}%, RAM: {ram}%, Disk: {disk}%" 
+
+# if __name__=="__main__": 
+#     print(system_status())
